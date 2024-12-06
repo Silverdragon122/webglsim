7cb3d1b85b4a995bfb6f34f5476cd28ed828ed9a
David Grinberg <101513517+Silverdragon122@users.noreply.github.com>
2024-12-05 21:40:17 -0500

    Last stuff b4 submission for real

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
   Routes,
   Navigate
 } from 'react-router-dom';
-import Tutorial from './pages/Tutorial'; // Import the new Tutorial component
 import Room from './pages/Room'; // Import the new Room component
 import NotFound from './pages/404'; // Import the new 404 component
 
@@ -12,8 +11,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/tutorial" />} />
-        <Route path="/tutorial" element={<Tutorial />} />
+        <Route path="/" element={<Navigate to="/room" />} />
         <Route path="/room" element={<Room />} />
         <Route path="*" element={<NotFound />} /> {/* Add the 404 route */}
       </Routes>
